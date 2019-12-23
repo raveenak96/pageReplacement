8e3f8d3fd69843aa0a253511b549886d6f83ca56
Raveena Kshatriya <raveenak96@gmail.com>
2019-12-22 21:27:59 -0500

    Add files via upload

---
+++ pager-lru.c
@@ -0,0 +1,81 @@
+/*
+ * File: pager-lru.c
+ * Author:       Andy Sayler
+ *               http://www.andysayler.com
+ * Adopted From: Dr. Alva Couch
+ *               http://www.cs.tufts.edu/~couch/
+ *
+ * Project: CSCI 3753 Programming Assignment 3
+ * Create Date: Unknown
+ * Modify Date: 2012/04/03
+ * Description:
+ * 	This file contains an lru pageit
+ *      implmentation.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "simulator.h"
+
+void pageit(Pentry q[MAXPROCESSES]) {
+
+    /* This file contains the stub for an LRU pager */
+    /* You may need to add/remove/modify any part of this file */
+    
+    /* Static vars */
+    static int initialized = 0;
+    static int tick = 1; // artificial time
+    static int timestamps[MAXPROCESSES][MAXPROCPAGES];
+
+    /* Local vars */
+    int proctmp;
+    int pagetmp;
+    int proc;
+    int pc;
+    int page;
+    int lru;
+    int l_timestamp;
+
+    /* initialize static vars on first run */
+    if(!initialized){
+	for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
+	    for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++){
+		  timestamps[proctmp][pagetmp] = 0;
+	    }
+	}
+	initialized = 1;
+    }
+
+    /* TODO: Implement LRU Paging */
+    for(proc=0; proc<MAXPROCESSES; proc++) {
+        pc = q[proc].pc;
+        page = pc/PAGESIZE;
+
+        if(q[proc].active) {
+            //update the timestamp for the process and page
+            timestamps[proc][page] = tick;
+
+
+        if(!q[proc].pages[page]) {
+            //try to page it in
+            if(!pagein(proc,page)) {
+                //find the least recently used page and page it out
+                l_timestamp = -1;
+                for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++) {
+
+                    if(q[proc].pages[pagetmp] && (tick-timestamps[proc][pagetmp]) > l_timestamp) {
+                        
+                        l_timestamp = tick-timestamps[proc][pagetmp];
+                        lru = pagetmp;
+                    }
+                }
+                pageout(proc,lru);
+            }
+        } 
+        }   
+    }
+
+    /* advance time for next pageit iteration */
+    tick++;
+}
---
+++ pager-predict.c
@@ -0,0 +1,210 @@
+/*
+ * File: pager-predict.c
+ * Author:       Andy Sayler
+ *               http://www.andysayler.com
+ * Adopted From: Dr. Alva Couch
+ *               http://www.cs.tufts.edu/~couch/
+ *
+ * Project: CSCI 3753 Programming Assignment 3
+ * Create Date: Unknown
+ * Modify Date: 2012/04/03
+ * Description:
+ * 	This file contains a predictive pageit
+ *      implmentation.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "simulator.h"
+
+//used to store page history to create our pattern
+typedef struct _page_history {
+
+    int nextPage[MAXPROCPAGES];
+    int numPages;
+} PageHistory;
+
+//holds the pattern of pages for each of our processes
+typedef struct _page_pattern {
+    PageHistory pageHistory[MAXPROCPAGES];
+    int prevPage;
+} PagePattern;
+
+//created a struct for a process, page pair for easier access
+typedef struct _proc_page {
+    int proc;
+    int page;
+} ProcPage;
+
+static PagePattern pattern[MAXPROCESSES];
+
+//use our knowledge to decide which pages to page out/in
+void performPaging(ProcPage* needed, ProcPage* neededSoon, ProcPage* remove, int numNeeded, int numNeededSoon, int numRemove,Pentry* q) {
+
+    int needIdx,needSIdx,curProc,curPage;
+    int pagedOut = 0;
+    //attempt to page in the pages needed now, otherwise page out one of our removable pages
+    for(needIdx=0;needIdx<numNeeded;needIdx++) {
+        curProc = needed[needIdx].proc;
+        curPage = needed[needIdx].page;
+        if(!q[curProc].pages[curPage]) {
+            if(!pagein(curProc,curPage)) {
+
+                if(pagedOut >= numRemove) {
+                    break;
+                } else {
+                    pageout(remove[pagedOut].proc,remove[pagedOut].page);
+                    pagedOut++;
+                }
+            }
+            
+        }
+    }
+    //attempt to page in pages we will need soon, otherwise page out one of our removable ones
+    for(needSIdx=0;needSIdx<numNeededSoon;needSIdx++) {
+        curProc = neededSoon[needSIdx].proc;
+        curPage = neededSoon[needSIdx].page;
+        if(!q[curProc].pages[curPage]) {
+            if(!pagein(curProc,curPage)) {
+                if(pagedOut >= numRemove) {
+                    break;
+                } else {
+                
+                    pageout(remove[pagedOut].proc,remove[pagedOut].page);
+                    pagedOut++;
+                } 
+            }
+
+        }
+    }
+
+}
+
+void pageit(Pentry q[MAXPROCESSES]) {
+
+    /* This file contains the stub for a predictive pager */
+    /* You may need to add/remove/modify any part of this file */
+
+    /* Static vars */
+    static int initialized = 0;
+    static int tick = 1; // artificial time
+
+    /* Local vars */
+    int proc;
+    int page;
+    int pc;
+    PageHistory procPgHist;
+    /* initialize static vars on first run */
+    if(!initialized){
+	/* Init complex static vars here */
+        for(proc=0;proc < MAXPROCESSES;proc++) {
+            for(page=0;page<MAXPROCPAGES;page++) {
+                pattern[proc].pageHistory[page].numPages = 0;
+            }
+        }
+	   initialized = 1;
+    }
+
+    //pages we don't need and able to swap out
+    int numRemove = 0;
+    ProcPage remove[MAXPROCPAGES*MAXPROCESSES];
+    
+
+    //pages that will be needed soon
+    int numNeededSoon = 0;
+    ProcPage neededSoon[MAXPROCPAGES*MAXPROCESSES];
+
+    //update the page pattern/history
+    //for each process add that current page comes after previous page
+    int prevPage,presPage,tmppage;
+    for(proc=0;proc<MAXPROCESSES;proc++) {
+
+        prevPage = pattern[proc].prevPage;
+        presPage = q[proc].pc / PAGESIZE;
+        if(presPage != prevPage) {
+            PageHistory* next = &(pattern[proc].pageHistory[prevPage]);
+            int added = 0;
+            for(tmppage=0;tmppage < next->numPages;tmppage++) {
+                if(next->nextPage[tmppage]==presPage) {
+                    added = 1;
+                    break;
+                }
+            }
+            if(!added) {
+                next->nextPage[next->numPages] = presPage;
+                (next->numPages)++;
+            }
+            
+        }
+        pattern[proc].prevPage = presPage;
+    }
+
+    //pages needed now
+    int numNeeded = 0;
+    ProcPage needed[MAXPROCESSES];
+    
+    //updating the needed,neededSoon, and remove data structures
+    int nSIndex,tmppage2;
+    for(proc=0;proc<MAXPROCESSES;proc++) {
+        pc = q[proc].pc;
+        page = pc/PAGESIZE;
+
+        if(q[proc].active) {
+            //update which pages are needed now (the current page)
+            needed[numNeeded].proc = proc;
+            needed[numNeeded].page = page;
+            numNeeded++;
+            //update which ones will be needed soon
+            procPgHist = pattern[proc].pageHistory[page];
+            for(nSIndex=0;nSIndex < procPgHist.numPages;nSIndex++) {
+                neededSoon[numNeededSoon].proc = proc;
+                neededSoon[numNeededSoon].page = procPgHist.nextPage[nSIndex];
+                numNeededSoon++;
+            }
+            //set our pages we can potentially remove (not needed now or soon)
+            for(tmppage2=0;tmppage2<q[proc].npages;tmppage2++) {
+
+                if(q[proc].pages[tmppage2]) {
+
+                    //make sure pages aren't currently needed
+                    int swappable = 1;
+                    int neededIdx,neededSIdx;
+                    for(neededIdx= numNeeded-1;neededIdx>=0;neededIdx--) {
+                        if(needed[neededIdx].proc==proc) {
+                            if(needed[neededIdx].page==tmppage2) {
+                                swappable = 0;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+
+                    //make sure pages won't be used soon
+                    for(neededSIdx=numNeededSoon-1;neededSIdx>=0;neededSIdx--) {
+                        if(neededSoon[neededSIdx].proc==proc) {
+                            if(swappable && neededSoon[neededSIdx].page==tmppage2) {
+                                swappable = 0;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+
+                    //pages not needed now or soon 
+                    if(swappable) {
+                        remove[numRemove].page = tmppage2;
+                        remove[numRemove].proc = proc;
+                        numRemove++;
+                    }
+
+                }
+            }
+
+
+        }
+    }
+    performPaging(needed,neededSoon,remove,numNeeded,numNeededSoon,numRemove,q);
+    /* advance time for next pageit iteration */
+    tick++;
+}
