a449060d42eeb568656f38d2a03b17200b9d0aac
Raveena Kshatriya <raveenak96@gmail.com>
2019-12-22 21:28:48 -0500

    Update pager-predict.c

--- pager-predict.c
+++ pager-predict.c
@@ -1,17 +1,4 @@
-/*
- * File: pager-predict.c
- * Author:       Andy Sayler
- *               http://www.andysayler.com
- * Adopted From: Dr. Alva Couch
- *               http://www.cs.tufts.edu/~couch/
- *
- * Project: CSCI 3753 Programming Assignment 3
- * Create Date: Unknown
- * Modify Date: 2012/04/03
- * Description:
- * 	This file contains a predictive pageit
- *      implmentation.
- */
+
 
 #include <stdio.h>
 #include <stdlib.h>
